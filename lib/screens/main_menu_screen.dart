a551333ad6de4a42e0c07596b0cfa5727bec6ea8
Momen-Assaf <momenssf@gmail.com>
2024-07-20 12:58:57 +0300

    for later

---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart'
+
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
